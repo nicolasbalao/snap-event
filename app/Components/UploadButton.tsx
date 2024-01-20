0f3142f245e1efe525d5842996734aa7c660b658
nicolas balao <nicolasbalao.pro@gmail.com>
2024-01-20 18:35:44 +0100

    ref: Refactor component and move to the root page + adding refreshing after uploading

--- app/Components/UploadButton.tsx
+++ app/Components/UploadButton.tsx
@@ -1,12 +1,20 @@
 "use client";
 import { CldUploadButton } from "next-cloudinary";
 import React from "react";
+import { useRouter } from "next/navigation";
 
 function UploadButton() {
+  const router = useRouter();
   return (
     <CldUploadButton
       uploadPreset={process.env.NEXT_PUBLIC_CLOUDINARY_PRESET_NAME}
       className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      onSuccess={() => {
+        // TODO: Find better solution for handle the reload
+        setTimeout(() => {
+          router.refresh();
+        }, 2000);
+      }}
     />
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,39 @@
-"use client";
-import { CldImage, CldUploadButton } from "next-cloudinary";
+import { v2 as cloudinary } from "cloudinary";
+import CCldImage from "./Components/CCldImage";
+import UploadButton from "./Components/UploadButton";
 
-export default function Home() {
+type imageData = {
+  public_id: string;
+};
+
+export default async function GalleryPage() {
+  const { resources } = await cloudinary.search
+    .expression("resource_type:image")
+    .sort_by("created_at", "desc")
+    .max_results(30)
+    .execute();
+
   return (
     <main>
-      <CldImage
-        width={900}
-        height={900}
-        src="https://res.cloudinary.com/ddomuvkaw/image/upload/v1704475945/cld-sample-5.jpg"
-        alt="Cloudinary Sample Image"
-      />
+      <div className="flex justify-between items-center mb-6">
+        <h1 className="text-3xl">Gallery page</h1>
+        <UploadButton />
+        <button className="bg-lime-600 text-white p-2 rounded">
+          <a href="/link">Create link</a>
+        </button>
+      </div>
+      <div className="grid grid-cols-4 gap-4">
+        {resources &&
+          resources.map((image: imageData) => (
+            <CCldImage
+              src={image.public_id}
+              alt="image"
+              height={100}
+              width={100}
+              key={image.public_id}
+            />
+          ))}
+      </div>
     </main>
   );
 }
