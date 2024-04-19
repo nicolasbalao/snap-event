f86adf2e81327916a0e442d5f8ad68f8294ab272
nicolas balao <nicolasbalao.pro@gmail.com>
2024-04-19 11:32:10 +0200

    fix deployement

--- app/magic-link/generate/page.tsx
+++ app/magic-link/generate/page.tsx
@@ -1,4 +1,4 @@
-export async function MagicLinkPage() {
+export default async function MagicLinkPage() {
   return (
     <div className="flex items-center justify-center h-full w-full p-6 bg-orange-100 text-purple-900 text-opacity-70">
       <h1>Generate Magic Link</h1>
