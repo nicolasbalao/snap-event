9141256be8772af8b013399a3583d448a641307c
nicolas balao <nicolasbalao.pro@gmail.com>
2024-04-19 10:47:32 +0200

    #8 ref(tags): Normalize tags for improve search features: trim and lowercase theme

--- actions/add-tags.action.ts
+++ actions/add-tags.action.ts
@@ -8,8 +8,10 @@ export default async function addTags(publicId: string, tags: string[]) {
     throw new Error("publicId is required");
   }
 
+  const normalizedTags = tags.map((tag) => tag.trim().toLowerCase());
+
   const { resources } = await cloudinary.api.update(publicId, {
-    tags: tags,
+    tags: normalizedTags,
   });
 
   return resources;
