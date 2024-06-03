8e755ff6b4cb8f35addbc07c50aa1a736149f2e8
nicolas balao <nicolasbalao.pro@gmail.com>
2024-06-03 17:45:37 +0200

    #23 End page detail photo when reload

--- app/photos/[public_id]/page.tsx
+++ app/photos/[public_id]/page.tsx
@@ -1,15 +1,19 @@
 import Link from "next/link";
 import PhotoDetails from "../../../components/PhotoDetail";
+import { ChevronLeft } from "lucide-react";
 
 export default async function PhotoDetailsPage({ params }: any) {
   const { public_id } = params;
 
   return (
-    <section className="h-screen w-screen flex justify-center items-center  ">
-      <Link href="/" className="self-start">
-        Gallerie
-      </Link>
-      <PhotoDetails public_id={public_id} />
+    <section className="h-screen w-screen flex justify-center items-center">
+      <div className="flex flex-col md:justify-center items-center gap-2">
+        <Link href="/" className="self-start flex items-center ">
+          <ChevronLeft />
+          Gallerie
+        </Link>
+        <PhotoDetails public_id={public_id} />
+      </div>
     </section>
   );
 }
